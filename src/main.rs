80d5052b422a6c8112df695d4a1b3402244d6161
tanrilarbahcesi <tanrilarbahcesi@proton.me>
2024-12-21 11:52:01 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let layout = std::fs::read_to_string("pages/layout.html").unwrap();
-    for file in std::fs::read_dir("pages/content").unwrap() {
+    for file in std::fs::read_dir("pages/content").unwrap() {        
         let content = std::fs::read_to_string(file.as_ref().unwrap().path().to_str().unwrap()).unwrap();
         let page = content_reader(content);
         let result_page = layout
@@ -8,8 +8,9 @@ fn main() {
             .replace("{{description}}", &page.description)
             .replace("{{keywords}}", &page.keywords)
             .replace("{{content}}", &page.content);
-        std::fs::write(format!("release/{}", file.unwrap().path().file_name().unwrap().to_str().unwrap()), result_page).unwrap();
+        std::fs::write(format!("release/{}", file.as_ref().unwrap().path().file_name().unwrap().to_str().unwrap()), result_page).unwrap();        
     }
+    generate_sitemap();
     println!("Done!");
 }
 
@@ -18,6 +19,27 @@ struct Page {
     description: String,
     keywords: String,
     content: String
+}
+
+fn generate_sitemap() {
+    let lastmod = chrono::Utc::now().format("%Y-%m-%dT%H:%M:%S+00:00").to_string();
+    let mut sitemap = String::from(r#"<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">"#);
+    for file in std::fs::read_dir("pages/content").unwrap() {
+        let filepath = file.unwrap().path();
+        let filename = filepath.file_name().unwrap().to_str().unwrap();
+        let priority = if filename == "index.html" { "1.0" } else { "0.8" };
+        sitemap.push_str(format!("
+        <url>
+            <loc>https://tanrilarbahcesi.com/{}</loc>
+            <lastmod>{}</lastmod>
+            <priority>{}</priority>
+        </url>
+        ", filename, lastmod, priority).as_str());
+    }
+    sitemap.push_str(r#"
+    </urlset>"#);
+    std::fs::write(format!("release/sitemap.xml"), sitemap).unwrap();
 }
 
 fn content_reader(content: String) -> Page {
